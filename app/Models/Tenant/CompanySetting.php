ec4e796db5cea16fb1bd5bea3a02e005477633e8
Ermi-HST <ermias.tadesse@hst-et.com>
2024-03-01 16:13:06 +0300

    Company Setting is Done!!!

--- app/Models/Tenant/CompanySetting.php
+++ app/Models/Tenant/CompanySetting.php
@@ -4,11 +4,10 @@ namespace App\Models\Tenant;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanySetting extends Model
 {
-    use SoftDeletes;
+    
     use HasFactory;
 
     protected $fillable = ['company_id', 'name', 'description', 'value'];
