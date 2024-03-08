69cdb1bb03c8ab42b1d8ee7ae1f19e3309cf6320
azazhwuletaw <azazh.wuletawu@hst-et.com>
2024-03-08 13:47:38 +0300

    Api's for deduction

---
+++ app/Http/Resources/Finance/DeductionResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\Finance;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DeductionResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'deduction_type' => new DeductionTypeResource($this->deductionType), // Assuming DeductionTypeResource exists
+            'value_type' => $this->value_type,
+            'value' => $this->value,
+            'status' => $this->status,
+
+        ];
+    }
+}
--- app/Models/Tenant/Deduction.php
+++ app/Models/Tenant/Deduction.php
@@ -4,14 +4,21 @@ namespace App\Models\Tenant;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Deduction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
     protected $fillable = [
         'name',
+        'deduction_type_id',
         'value_type',
         'value',
         'status',
     ];
+
+    public function deductionType()
+    {
+        return $this->belongsTo(DeductionType::class);
+    }
 }
