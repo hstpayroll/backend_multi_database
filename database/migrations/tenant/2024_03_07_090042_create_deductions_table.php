bf5e77971fe841534e3fdf9e59199f5741bcdb6e
hstpayroll <yetimeshet.tadesse@hst-et.com>
2024-03-08 11:54:41 +0300

    deduction_relashinship_updated

--- database/migrations/tenant/2024_03_07_090042_create_deductions_table.php
+++ database/migrations/tenant/2024_03_07_090042_create_deductions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('deductions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('deduction_type_id')->constrained('deduction_types')->onDelete('restrict')->onUpdate('cascade');
             $table->boolean('value_type')->default(0); //0 for fixed value, 1 for percentage
             $table->decimal('value', 10, 2)->default(0)->nullable(); //how can it deduct from your salary if it is in % deduct in % or the actual value
             $table->tinyInteger('status')->default(1);
--- database/migrations/tenant/2024_03_07_091640_create_deduction_transactions_table.php
+++ database/migrations/tenant/2024_03_07_091640_create_deduction_transactions_table.php
@@ -14,12 +14,14 @@ return new class extends Migration
         Schema::create('deduction_transactions', function (Blueprint $table) {
             $table->foreignId('payroll_period_id')->constrained('payroll_periods')->onDelete('restrict')->onUpdate('cascade');
             $table->foreignId('employee_id')->constrained('employees')->onDelete('restrict')->onUpdate('cascade');
-            $table->foreignId('deduction_type_id')->constrained('deduction_types')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreignId('deduction_id')->constrained('deductions')->onDelete('restrict')->onUpdate('cascade');
             $table->decimal('amount_deducted', 10, 2);
             $table->decimal('outstanding_amount', 10, 2);
             $table->boolean('is_partial')->default(false);
             $table->boolean('is_missed')->default(false);
             $table->date('start_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
