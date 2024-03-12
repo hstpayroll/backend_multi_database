8c3d605bd0b98a4889d5b84468a988c5a8ced328
azazhwuletaw <azazh.wuletawu@hst-et.com>
2024-03-12 16:14:12 +0300

    deduction transaction resource and request

--- app/Models/Tenant/DeductionTransaction.php
+++ app/Models/Tenant/DeductionTransaction.php
@@ -4,8 +4,34 @@ namespace App\Models\Tenant;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DeductionTransaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'payroll_period_id',
+        'employee_id',
+        'deduction_id',
+        'amount_deducted',
+        'outstanding_amount',
+        'is_partial',
+        'is_missed',
+        'start_date',
+    ];
+
+    public function payrollPeriod()
+    {
+        return $this->belongsTo(PayrollPeriod::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function deduction()
+    {
+        return $this->belongsTo(Deduction::class);
+    }
 }
