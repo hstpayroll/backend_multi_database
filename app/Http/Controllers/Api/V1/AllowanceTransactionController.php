635d191b5b523c0b8663199ea7c7238ede7609e2
Ermi-HST <ermias.tadesse@hst-et.com>
2024-03-12 12:32:39 +0300

    store and update deduction make status nullable

--- app/Http/Controllers/Api/V1/AllowanceTransactionController.php
+++ app/Http/Controllers/Api/V1/AllowanceTransactionController.php
@@ -13,29 +13,45 @@ use App\Http\Resources\Finance\AllowanceTransactionResource;
 
 class AllowanceTransactionController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $allowanceTransaction =  AllowanceTransaction::with(['payrollPeriod', 'employee', 'allowanceType'])->paginate(10);
+        $allowanceTransaction =  AllowanceTransaction::with(['payrollPeriods', 'employees', 'allowanceTypes'])->paginate(10);
         return AllowanceTransactionResource::collection($allowanceTransaction);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
+    // public function store(Request $request)
     public function store(StoreAllowanceTransactionRequest $request)
     {
         $allowanceTransaction = AllowanceTransaction::create($request->validated());
         return new AllowanceTransactionResource($allowanceTransaction);
     }
 
+    /**
+     * Display the specified resource.
+     */
     public function show(AllowanceTransaction $allowanceTransaction)
     {
         return new AllowanceTransactionResource($allowanceTransaction);
     }
 
+    /**
+     * Update the specified resource in storage.
+     */
     public function update(UpdateAllowanceTransactionRequest $request, AllowanceTransaction $allowanceTransaction)
     {
         $allowanceTransaction->update($request->validated());
         return new AllowanceTransactionResource($allowanceTransaction);
     }
 
+    /**
+     * Remove the specified resource from storage.
+     */
     public function destroy(AllowanceTransaction $allowanceTransaction)
     {
         $allowanceTransaction->delete();
