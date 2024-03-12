c93b0aa798d4196967c3cadc457bfb0916f4f02f
Ermi-HST <ermias.tadesse@hst-et.com>
2024-03-12 10:27:51 +0300

    Allowance transaction

--- app/Http/Resources/Finance/AllowanceTransactionResource.php
+++ app/Http/Resources/Finance/AllowanceTransactionResource.php
@@ -15,7 +15,7 @@ class AllowanceTransactionResource extends JsonResource
             'payrollPeriods' => new PayrollPeriodResource($this->payrollPeriod),
             'amount' => $this->amount,
             'taxable_amount' => $this->taxable_amount,
-            'tax_free_amount' => $this->tax_free_amount,
+            'tax_free_amount' => $this->non_taxable_amount,
             'is_day_based' => $this->value_type == 0 ? 'Yes' : 'No',
             'start_date' =>  $this->start_date,
             'status' => $this->status == 1 ? 'active' : 'inactive',
