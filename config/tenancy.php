dccdf572218b7bb7b9a1e7a7c45fab70e0067d62
hstpayroll <yetimeshet.tadesse@hst-et.com>
2024-03-25 14:02:34 +0300

    for local deployemnt

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,11 +18,11 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        // 'localhost',
-        // '127.0.0.1',
+        'localhost',
+        '127.0.0.1',
         'hstpayroll.et',
         // 'pay.hst-et.com'
-        // '10.1.11.29',
+        '10.1.11.29',
         // 'multidb.local'
     ],
 
--- database/seeders/TenantUserSeeder.php
+++ database/seeders/TenantUserSeeder.php
@@ -11,16 +11,6 @@ class TenantUserSeeder extends Seeder
 
     public function run(): void
     {
-
-        //user 4
-        $user4 =  User::create([
-            'name' => "User 4",
-            'email' => "user2@gmail.com",
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ])->assignRole(['admin']);
-        $user4->givePermissionTo(['edit_profile', 'change_password']);
-
         //user 5
         $user5 =  User::create([
             'name' => "User 5",
@@ -30,7 +20,6 @@ class TenantUserSeeder extends Seeder
         ])->assignRole(['accountant']);
         $user5->givePermissionTo(
             [
-                'edit_profile', 'change_password',
                 'employee_index', 'employee_store', 'employee_show', 'employee_update', 'employee_destroy',
                 'bank_index', 'bank_store', 'bank_show', 'bank_update', 'bank_destroy',
                 'branch_index', 'branch_store', 'branch_show', 'branch_update', 'branch_destroy',
