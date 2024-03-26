4f648af430975461a6dc13ada0aa3b87a75d81e8
Ermi-HST <ermias.tadesse@hst-et.com>
2024-03-26 11:54:01 +0300

    change

--- database/migrations/tenant/2024_03_07_090042_create_deductions_table.php
+++ database/migrations/tenant/2024_03_07_090042_create_deductions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('main_deductions', function (Blueprint $table) {
+        Schema::create('deductions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('employee_id')->constrained()->onDelete('cascade');
             $table->foreignId('deduction_type_id')->constrained()->onDelete('cascade');
